2942e3641379a4227d81dfb34e171642fa303388
AriCastillo99 <89433795+AriCastillo99@users.noreply.github.com>
2021-11-29 23:15:42 -0800

    Add files via upload

---
+++ ChefPatron.java
@@ -0,0 +1,22 @@
+
+
+public class ChefPatron extends ChefPro implements Funciones{
+	
+	public ChefPatron() {
+		super();
+	}
+	public String Cortarcarne() {
+		return ("Chef Robot >> "+ nombre +" << Trabajando en una de mis funciones 8) >>");
+	}
+	public String Picarverdura() {
+		return ("Chef Robot >> " + nombre +" << Trabajando en una de mis funciones 8) >>");
+	}
+	public String cocinar() {
+		return ("Chef Robot >> "+ nombre +" << Trabajando en una de mis funciones 8) >>");
+	}
+	public String Servir() {
+		return ("Chef Robot >> "+ nombre +" << Trabajando en una de mis funciones 8) >>");
+	}
+	public String Calentar() {
+		return ("Chef Robot >> "+ nombre +" << Trabajando en una de mis funciones 8( >>");	}
+}
---
+++ RobotChef2.java
@@ -0,0 +1,27 @@
+public class RobotChef2 extends ChefPro implements Funciones{
+	public boolean acci = false;
+	public boolean acco;
+	public RobotChef2() {
+		super();
+	}
+	public String Cortarcarne() {
+		acci = true;
+		return ("Apoyo 2 >> "+ nombre +" No puedo trabajar en eso, no es de mis funciones 8( ");
+	}
+	public String Picarverdura() {
+		acci = true;
+		return ("Apoyo 2 >> "+ nombre +" << No puedo trabajar en eso, no es de mis funciones 8( >>");
+	}
+	public String cocinar() {
+		acci = true;
+		return ("Apoyo 2 >> "+ nombre +" << Trabajando en una  de mis funciones 8) >>");
+	}
+	public String Servir() {
+		acci = true;
+		return (" Apoyo 2 >> "+ nombre +" << No puedo trabajar en eso, no es de mis funciones 8( >>");
+	}
+	public String Calentar() {
+		acci = true;
+		return ("Apoyo 2 >> "+ nombre +" << Trabajando en una  de mis funciones 8) >>");
+	}
+}
